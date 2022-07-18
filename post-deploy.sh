e4a50f29346f440534353b1b5f0f71ca770f3c73
Navratan Lal Gupta <37469234+navilg@users.noreply.github.com>
2022-07-18 18:13:16 +0530

    Separate Nginx docker-compose

--- post-deploy.sh
+++ post-deploy.sh
@@ -5,3 +5,6 @@ docker exec -it transmission chown -R 1000:1000 /downloads/movies
 
 docker exec -it transmission mkdir -p /downloads/tvshows
 docker exec -it transmission chown -R 1000:1000 /downloads/tvshows
+
+
+docker ps | grep nginx && docker cp nginx.conf nginx:/etc/nginx/conf.d/default.conf && docker exec -it nginx nginx -s reload
