a7803a29e576ff314be3456d904a6b3aabd8a4f6
Navratan Lal Gupta <navilg0409@gmail.com>
2022-07-18 16:39:23 +0530

    Add media directory creation

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker exec -it transmission mkdir -p /downloads/movies
 docker exec -it transmission chown -R 1000:1000 /downloads/movies
 
+docker exec -it transmission mkdir -p /downloads/tvshows
 docker exec -it transmission chown -R 1000:1000 /downloads/tvshows
